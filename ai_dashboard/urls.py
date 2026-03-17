a47afcb363f77b45f8ebff95644ab8d3ffe53c92
lashaa06 <maghei0706@gmail.com>
2026-03-17 15:01:38 +0530

    fixed register route

--- ai_dashboard/urls.py
+++ ai_dashboard/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', views.login_view),
+    path('login/', views.login_view),
     path('register/', views.register_view),
+    path('dashboard/', views.home),
     path('logout/', views.logout_view),
 
     path('dashboard/', views.home),
